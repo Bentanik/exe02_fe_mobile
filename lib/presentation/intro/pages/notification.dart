8dfe0d937cfd9539a68a9beb6a43d3ef782e822c
lamtanphat2202 <tphatt22@gmail.com>
2025-01-18 10:47:45 +0700

    finished notification page

---
+++ lib/presentation/intro/pages/notification.dart
@@ -0,0 +1,80 @@
+import 'package:exe02_fe_mobile/common/helpers/routes.dart';
+import 'package:exe02_fe_mobile/common/widget/button.dart';
+import 'package:exe02_fe_mobile/common/widget/input_field.dart';
+import 'package:exe02_fe_mobile/common/widget/notification_card.dart';
+import 'package:exe02_fe_mobile/core/configs/assets/app_images.dart';
+import 'package:exe02_fe_mobile/core/configs/assets/app_vectors.dart';
+import 'package:exe02_fe_mobile/presentation/intro/pages/success.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NotificationPage extends StatelessWidget {
+  const NotificationPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Notification')),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 50),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //Today
+                Text(
+                  'Hôm nay',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const SizedBox(height: 15),
+                NotificationCard(
+                    title: 'khóa học mới',
+                    desc: 'Vừa ra vào 1 giờ trước',
+                    leading: Icon(FontAwesomeIcons.addressBook),
+                ),
+                const SizedBox(height: 10),
+                NotificationCard(
+                  title: 'khóa học mới',
+                  desc: 'Vừa ra vào 1 giờ trước',
+                  leading: Icon(FontAwesomeIcons.addressBook),
+                ),
+                const SizedBox(height: 10),
+                NotificationCard(
+                  title: 'khóa học mới',
+                  desc: 'Vừa ra vào 1 giờ trước',
+                  leading: Icon(FontAwesomeIcons.addressBook),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Hôm qua',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const SizedBox(height: 15),
+                NotificationCard(
+                  title: 'khóa học mới',
+                  desc: 'Vừa ra vào 1 giờ trước',
+                  leading: Icon(FontAwesomeIcons.addressBook),
+                ),
+                const SizedBox(height: 10),
+                NotificationCard(
+                  title: 'khóa học mới',
+                  desc: 'Vừa ra vào 1 giờ trước',
+                  leading: Icon(FontAwesomeIcons.addressBook),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
